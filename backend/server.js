2b6824246da734767b95af9fad0f80a3f2f5caf4
ngan16052005 <ngancute16052005@gmail.com>
2026-05-22 11:10:53 +0700

    fix: express 5 wildcard route error

--- backend/server.js
+++ backend/server.js
@@ -1918,7 +1918,7 @@ if (process.env.NODE_ENV !== 'production') {
     startServer();
 }
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
