365ab1d34abee2ec6180e8d8c0a19490f1106fd8
ngan16052005 <ngancute16052005@gmail.com>
2026-05-23 10:02:37 +0700

    fix: target account dropdown specifically in checkAdmin

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1047,7 +1047,7 @@ function checkAdmin() {
     if (user && (user.userType == 1 || user.userType == 2)) {
         let node = document.createElement(`li`);
         node.innerHTML = `<a href="./admin.html"><i class="fa-light fa-gear"></i> Quản lý cửa hàng</a>`
-        document.querySelector('.header-middle-right-menu').prepend(node);
+        document.querySelector('#account-dropdown .header-middle-right-menu').prepend(node);
     }
 }
 
